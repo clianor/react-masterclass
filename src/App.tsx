4ce4b35d98134b747549e777231eceb6f719d923
clianor <ejrgus94160@estsecurity.com>
2022-01-15 23:17:26 +0900

    Introduction to Recoil part Two

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,7 @@ function App() {
 		<ThemeProvider theme={isDark ? darkTheme : lightTheme}>
 			<GlobalStyle />
 			<Router />
-			<ReactQueryDevtools initialIsOpen={true} />
+			<ReactQueryDevtools initialIsOpen={false} />
 		</ThemeProvider>
 	);
 }
