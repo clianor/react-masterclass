e50922a693eec5cec6e802cba0c42a65a882c654
김덕현 <oraclian@estsecurity.com>
2022-01-24 20:51:25 +0900

    Final Styles

--- src/components/DragabbleCard.tsx
+++ src/components/DragabbleCard.tsx
@@ -2,28 +2,32 @@ import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
 	border-radius: 5px;
 	margin-bottom: 5px;
-	padding: 10px 10px;
-	background-color: ${(props) => props.theme.cardColor};
+	padding: 10px;
+	background-color: ${(props) => (props.isDragging ? '#e4f2ff' : props.theme.cardColor)};
+	box-shadow: ${(props) =>
+		props.isDragging ? '0px 2px 5px rgba(0, 0, 0, 0.05)' : 'none'};
 `;
 
 interface IDragabbleCardProps {
-	index: number;
 	toDo: string;
+	index: number;
 }
 
-function DragabbleCard({ index, toDo }: IDragabbleCardProps) {
+function DragabbleCard({ toDo, index }: IDragabbleCardProps) {
 	return (
 		<Draggable draggableId={toDo} index={index}>
-			{(magic) => {
-				return (
-					<Card ref={magic.innerRef} {...magic.dragHandleProps} {...magic.draggableProps}>
-						{toDo}
-					</Card>
-				);
-			}}
+			{(magic, snapshot) => (
+				<Card
+					isDragging={snapshot.isDragging}
+					ref={magic.innerRef}
+					{...magic.dragHandleProps}
+					{...magic.draggableProps}>
+					{toDo}
+				</Card>
+			)}
 		</Draggable>
 	);
 }
