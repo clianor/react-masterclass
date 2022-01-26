4a7ce2c74ab33f338a60c87eacaad31bd717d790
김덕현 <oraclian@estsecurity.com>
2022-01-26 15:33:13 +0900

    Slider part Two

--- src/App.tsx
+++ src/App.tsx
@@ -19,58 +19,62 @@ const Box = styled(motion.div)`
 	position: absolute;
 	top: 100px;
 	display: flex;
-	align-items: center;
 	justify-content: center;
+	align-items: center;
 	font-size: 28px;
 	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const box = {
-	invisible: {
-		x: 500,
+	entry: (isBack: boolean) => ({
+		x: isBack ? -500 : 500,
 		opacity: 0,
 		scale: 0,
-	},
+	}),
 	visible: {
 		x: 0,
 		opacity: 1,
 		scale: 1,
 		transition: {
-			duration: 1,
+			duration: 0.3,
 		},
 	},
-	exit: {
-		x: -500,
+	exit: (isBack: boolean) => ({
+		x: isBack ? 500 : -500,
 		opacity: 0,
 		scale: 0,
 		transition: {
-			duration: 1,
+			duration: 0.3,
 		},
-	},
+	}),
 };
 
 function App() {
 	const [visible, setVisible] = useState(1);
-	const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-
+	const [back, setBack] = useState(false);
+	const nextPlease = () => {
+		setBack(false);
+		setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+	};
+	const prevPlease = () => {
+		setBack(true);
+		setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+	};
 	return (
 		<Wrapper>
-			<AnimatePresence>
-				{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(
-					(i) =>
-						i === visible && (
-							<Box
-								key={i.toString()}
-								variants={box}
-								initial="invisible"
-								animate="visible"
-								exit="exit">
-								{i}
-							</Box>
-						),
-				)}
-				<button onClick={nextPlease}>next</button>
+			<AnimatePresence custom={back}>
+				<Box
+					custom={back}
+					variants={box}
+					initial="entry"
+					animate="visible"
+					exit="exit"
+					key={visible}>
+					{visible}
+				</Box>
 			</AnimatePresence>
+			<button onClick={nextPlease}>next</button>
+			<button onClick={prevPlease}>prev</button>
 		</Wrapper>
 	);
 }
