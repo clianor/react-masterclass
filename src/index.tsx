6cece14b1e8e6719a22cfa9f984e0b3ed6e3455d
김덕현 <oraclian@estsecurity.com>
2022-01-27 13:50:57 +0900

    Introduction

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { RecoilRoot } from 'recoil';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import App from './App';
-import { darkTheme } from './theme';
+import { theme } from './theme';
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -73,7 +73,7 @@ const GlobalStyle = createGlobalStyle`
 ReactDOM.render(
 	<React.StrictMode>
 		<RecoilRoot>
-			<ThemeProvider theme={darkTheme}>
+			<ThemeProvider theme={theme}>
 				<GlobalStyle />
 				<App />
 			</ThemeProvider>
